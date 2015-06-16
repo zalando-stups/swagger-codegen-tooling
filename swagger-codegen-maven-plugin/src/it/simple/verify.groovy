77b4ba755b2da5a446b60ae9a5bb87213cb6c087
jbellmann <joerg.bellmann@zalando.de>
2015-06-16 20:37:55 +0200

    update to new output-folder 'swagger-codegen'

--- swagger-codegen-maven-plugin/src/it/simple/verify.groovy
+++ swagger-codegen-maven-plugin/src/it/simple/verify.groovy
@@ -1,14 +1,14 @@
 // api
-new File(basedir, "target/generated-sources/java/de/zalando/api/ApiException.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/ApiOriginFilter.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/ApiResponseMessage.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/NotFoundException.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/PetApi.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/StoreApi.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/api/UserApi.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/ApiException.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/ApiOriginFilter.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/ApiResponseMessage.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/NotFoundException.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/PetApi.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/StoreApi.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/api/UserApi.java" ).exists();
 // model
-new File(basedir, "target/generated-sources/java/de/zalando/model/Category.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/model/Order.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/model/Pet.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/model/Tag.java" ).exists();
-new File(basedir, "target/generated-sources/java/de/zalando/model/User.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/model/Category.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/model/Order.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/model/Pet.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/model/Tag.java" ).exists();
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/model/User.java" ).exists();
