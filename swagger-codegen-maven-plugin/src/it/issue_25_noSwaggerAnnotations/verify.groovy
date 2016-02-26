b282aa4c71886d6ba6dff82b3f7770d9a5c6e0f8
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-26 10:53:39 +0100

    add simple tests for issue_25

---
+++ swagger-codegen-maven-plugin/src/it/issue_25_noSwaggerAnnotations/verify.groovy
@@ -0,0 +1,4 @@
+// api
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/wholesale/warehouseallocation/api/controller/WarehousesApi.java" ).exists();
+// model
+new File(basedir, "target/generated-sources/swagger-codegen/de/zalando/wholesale/warehouseallocation/api/model/Warehouse.java" ).exists();
